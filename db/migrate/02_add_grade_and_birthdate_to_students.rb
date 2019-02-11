64c5604d0ee7d035fef6f3ac1054e60818907284
Jane Venter <janeanneventer@gmail.com>
2019-02-11 18:32:26 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
  class CreateStudents02 < ActiveRecord::Migration
    def change
-     add_column(:students, :grade, :integer)
-     add_column(:students, :birthdate, :string)
+     add_column :students, :grade, :integer
+     add_column :students, :birthdate, :string
    end
  end
